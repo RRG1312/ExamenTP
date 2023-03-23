a2e15845683af85eb1e3ccfd78dcbe9bbb9f7dc0
ruben <ruben.ben111@gmail.com>
2023-03-23 20:20:06 +0100

    Ejercicio 2 terminado

--- src/main/java/Ejercicio2/Tablero.java
+++ src/main/java/Ejercicio2/Tablero.java
@@ -1,70 +1,86 @@
 package Ejercicio2;
 
-public class Tablero {
+import java.util.Random;
 
-    int[][] posiciones = new int[30][30];
+public class Tablero {
+    private static final int DIMENSION =30;
+    private int[][] estadoActual;
+    private int[][] estadoSiguiente;
     public Tablero(){
+        this.estadoSiguiente =new int[30][30];
+        this.estadoActual = new int[30][30];
+    }
+
+    public void leerEstadoActual(){
         for (int i = 0; i < 30; i++) {
             for (int j = 0; j <30 ; j++) {
-                if(Math.random()*11>3){
-                    this.posiciones[i][j]=1;
+                if(Math.random()<1){
+                    this.estadoActual[i][j]=1;
                 }else{
-                    this.posiciones[i][j]=0;
+                    this.estadoActual[i][j]=0;
                 }
             }
         }
     }
 
+    public void generarEstadoActualPorMontecarlo() {
+        Random random = new Random();
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                int r = random.nextInt(2);
+                estadoActual[i][j] = r;
+                int vecinosVivos = contarVecinosVivos(i, j);
+                if (r == 1 && (vecinosVivos < 2 || vecinosVivos > 3)) {
+                    estadoSiguiente[i][j] = 0;
+                } else if (r == 0 && vecinosVivos == 3) {
+                    estadoSiguiente[i][j] = 1;
+                } else {
+                    estadoSiguiente[i][j] = r;
+                }
+            }
+        }
+        this.estadoActual = this.estadoSiguiente;
+    }
+    public int contarVecinosVivos(int fila, int columna) {
+        int contador = 0;
+        for (int i = fila - 1; i <= fila + 1; i++) {
+            for (int j = columna - 1; j <= columna + 1; j++) {
+                if (i >= 0 && i < 30 && j >= 0 && j < 30 && !(i == fila && j == columna)) {
+                    contador += estadoActual[i][j];
+                }
+            }
+        }
+        return contador;
+    }
 
     @Override
     public String toString() {
         String res ="";
-        for (int i = 0; i <30; i++) {
+        for (int i = 0; i <DIMENSION; i++) {
             res+="\n";
-            for (int j = 0; j <30 ; j++) {
-                res+=this.posiciones[i][j]+" ";
+            for (int j = 0; j <DIMENSION ; j++) {
+                res+=this.estadoActual[i][j]+" ";
             }
         }
         return res;
     }
 
-    public static Tablero generarNuevoestado(Tablero tablero){
-        Tablero res = tablero;
-        int contador =0;
+    public void transitarAlEstadoSiguiente() {
+        int[][] temp = this.estadoActual;
+        this.estadoActual = this.estadoSiguiente;
+        estadoSiguiente = temp;
 
-        for (int i = 0; i <30 ; i++) {
-            for (int j = 0; j <30 ; j++) {
-
-                for (int k = -1; k <=1 ; k++) {
-                    for (int l = -1; l <=1 ; l++) {
-                        if(i+k>0 && i+k<30 && j+l>0 && j+l<30){
-                            if(tablero.posiciones[i+k][j+l]==1){
-                                contador++;
-                            }
-                        }
-                    }
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                int vecinosVivos = contarVecinosVivos(i, j);
+                if (estadoActual[i][j] == 1 && (vecinosVivos == 2 || vecinosVivos == 3)) {
+                    estadoSiguiente[i][j] = 1;
+                } else if (estadoActual[i][j] == 0 && vecinosVivos == 3) {
+                    estadoSiguiente[i][j] = 1;
+                } else {
+                    estadoSiguiente[i][j] = 0;
                 }
-
-                if(tablero.posiciones[i][j]==0){
-                    if(contador==3){
-                        res.posiciones[i][j]=1;
-                    }else{
-                        res.posiciones[i][j]=0;
-                    }
-                }else{
-                    if(contador ==2 || contador==3){
-                        res.posiciones[i][j]=1;
-                    }else{
-                        res.posiciones[i][j]=0;
-                    }
-                }
-
-                contador =0;
-
             }
         }
-
-
-        return res;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,31 @@
 import Ejercicio1.Matematicas;
 import Ejercicio1.Menu1;
+import Ejercicio2.Tablero;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Menu1.menu();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+       // try {
+        //    Menu1.menu();
+        //} catch (Exception e) {
+         //   System.out.println(e.getMessage());
+        //}
+
+
+        Tablero tablero = new Tablero();
+        System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
+        tablero.leerEstadoActual();
+        System.out.println(tablero);
+        for (int i = 0; i <= 5; i++) {
+            tablero.transitarAlEstadoSiguiente();
+            System.out.println(tablero);
         }
+        System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
+        tablero.generarEstadoActualPorMontecarlo();
+        System.out.println(tablero);
+        for (int i = 0; i <= 15; i++) {
+            tablero.transitarAlEstadoSiguiente();
+            System.out.println(tablero);
+        }
+
     }
 }
